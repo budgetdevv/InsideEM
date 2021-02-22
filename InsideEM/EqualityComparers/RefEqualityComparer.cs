5eea59e7ce04f8049dba160c43b3fbe650d638cd
BudgetDevv <budgetdevv@gmail.com>
2021-02-22 13:43:42 +0800

    Added RefEqualityComparer

---
+++ InsideEM/EqualityComparers/RefEqualityComparer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+
+namespace InsideEM.EqualityComparers
+{
+    public readonly struct RefDefaultEqualityComparer<T>: IInsideEqualityComparer<T>
+    {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public bool Equals(ref T First, ref T Second)
+        {
+            return Unsafe.AreSame(ref First, ref Second);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public int GetHashCode(ref T Item)
+        {
+            return EqualityComparer<T>.Default.GetHashCode(Item);
+        }
+    }
+}
